6e44e47cefc0d6ac48928742f806a6d643024294
Gustavo Barbosa <gusbdev@gmail.com>
2022-08-01 14:53:52 -0300

    feat: using local system system

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,10 +1,11 @@
+import {Text} from "@gus/design-system/components/Text"
 import {sum} from "@gus/utils/math/sum"
 
 export default function HomeScreen() {
     return (
-        <div>
-            <h1>Home do Projeto</h1>
+        <main>
+            <Text tag="h1">Home do Projeto</Text>
             <p>Importing my local module: @gus/utils/math/sum sum(2,2): {sum(2,2)}</p>
-        </div>
+        </main>
     );
 }
