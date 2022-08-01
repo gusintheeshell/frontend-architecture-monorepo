fb0569936dcc1cccd2bfecb49fc47e28c46f22cc
Gustavo Barbosa <gusbdev@gmail.com>
2022-08-01 14:15:53 -0300

    feat: add initial conf

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,10 @@
+import {sum} from "@gus/utils/math/sum"
+
 export default function HomeScreen() {
     return (
         <div>
             <h1>Home do Projeto</h1>
+            <p>Importing my local module: @gus/utils/math/sum sum(2,2): {sum(2,2)}</p>
         </div>
     );
 }
