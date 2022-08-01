a4ecc499f908271e9751cc942d6761ea43efd6ae
Gustavo Barbosa <gusbdev@gmail.com>
2022-08-01 14:50:35 -0300

    chore: add transpile module and local design-system

---
+++ projects/web-public/next.config.js
@@ -0,0 +1,12 @@
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([
+    "@gus/design-system",
+    "@gus/utils"
+]);
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withPlugins([withTM], {
+  trailingSlash: true
+});
